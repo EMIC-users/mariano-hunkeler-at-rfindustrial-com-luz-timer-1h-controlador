2b4f971bce2627276c2c1d6f5ca0c934a1d2d724
EMIC Compiler <emic-compiler@emic.io>
2026-03-10 22:14:18 -0300

    Code generation: 2026-03-11 01:14:18 UTC
    Errors: 0
    Warnings: 0
    
    ---
    Module: controlador
    Project: luz-timer-1h
    User: mariano-hunkeler-at-rfindustrial-com

---
+++ userFncFile.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include "inc/userFncFile.h"
+#include "inc/systemTimer.h"
+#include "inc/led_LED1.h"
+#include "inc/led_LED2.h"
+#include "inc/led_LED3.h"
+#include "inc/relay_Relay1.h"
+#include "inc/relay_Relay2.h"
+#include "inc/timer_api1.h"
+#include "inc/EMICBus.h"
+
+void onReset()
+{
+    setTime1(60000, 'T');
+}
+
+
+void etOut1()
+{
+    if (equals(/* unsupported parameter type: emic-data-variable */, 0))
+    {
+    }
+}
+
+
+
